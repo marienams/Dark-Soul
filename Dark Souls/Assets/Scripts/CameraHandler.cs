e4fd5df6fd063b9049615e654a89da9bb0f5828d
Mahima SIngh <mahima.ms0007@gmail.com>
2022-09-22 10:11:26 +0200

    adding camera collision handler
    camera goes through 3d objects and lerps to the player when colliding with another layer

--- Dark Souls/Assets/Scripts/CameraHandler.cs
+++ Dark Souls/Assets/Scripts/CameraHandler.cs
@@ -20,11 +20,16 @@ using UnityEngine;
         public float followSpeed = 0.1f;
         public float pivotSPeed = 0.03f;
 
+        private float targetPosition;
         private float defaultPosition;
         private float lookAngle;
         private float pivotAngle;
         public float minimumPivot = -35;
         public float maximumPivot = 35;
+
+        private float cameraSphereRadius;
+        private float cameraCollisionOffset = 0.2f;
+        private float minCameraCollissionOffset = 0.2f;
         
 
 
@@ -42,25 +47,52 @@ using UnityEngine;
             //smoothdamp is a lot smoother than lerp for camera movement
             //Vector3 targetPosition = Vector3.Lerp(myTransform.position, targetTransform.position, delta / followSpeed); // for camera movement around the player 
             myTransform.position = targetPosition;
+            CameraCollisionHandler(delta);
         }
 
         public void HandleRotation(float delta, float mouseXinput, float mouseYinput)
-    {
-        lookAngle += (mouseXinput * lookSpeed) / delta;
-        pivotAngle -= (mouseYinput * pivotSPeed) / delta;
-        pivotAngle = Mathf.Clamp(pivotAngle, minimumPivot, maximumPivot);
+        {
+            lookAngle += (mouseXinput * lookSpeed) / delta;
+            pivotAngle -= (mouseYinput * pivotSPeed) / delta;
+            pivotAngle = Mathf.Clamp(pivotAngle, minimumPivot, maximumPivot);
 
-        Vector3 rotation = Vector3.zero;
-        rotation.y = lookAngle;
-        Quaternion targetRotation = Quaternion.Euler(rotation);
-        myTransform.rotation = targetRotation;
+            Vector3 rotation = Vector3.zero;
+            rotation.y = lookAngle;
+            Quaternion targetRotation = Quaternion.Euler(rotation);
+            myTransform.rotation = targetRotation;
 
-        rotation = Vector3.zero;
-        rotation.x = pivotAngle;
+            rotation = Vector3.zero;
+            rotation.x = pivotAngle;
 
-        targetRotation = Quaternion.Euler(rotation);
-        cameraPivotTransform.localRotation = targetRotation;
-    }
+            targetRotation = Quaternion.Euler(rotation);
+            cameraPivotTransform.localRotation = targetRotation;
+        }
+
+        public void CameraCollisionHandler (float delta)
+        {
+            targetPosition = defaultPosition;
+            RaycastHit hit; // structure used to get back info from raycast
+            Vector3 direction = cameraTransform.position - cameraPivotTransform.position;
+            direction.Normalize(); // to smoothen out any glitch
+            //shpere cast function
+            if(Physics.SphereCast(cameraPivotTransform.position, cameraSphereRadius, direction, out hit, Mathf.Abs(targetPosition), ignoreLayers))
+            {
+                float dis = Vector3.Distance(cameraPivotTransform.position, hit.point);
+                targetPosition = -(dis - cameraCollisionOffset);
+
+            }
+             if(Mathf.Abs(targetPosition) < minCameraCollissionOffset)
+            {
+                targetPosition = -minCameraCollissionOffset;
+
+            }
+
+            cameraTransformPosition.z = Mathf.Lerp(cameraTransform.localPosition.z, targetPosition, delta / 0.2f);
+            cameraTransform.localPosition = cameraTransformPosition;
+
+
+            }
+
         
 
     }
